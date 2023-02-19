5da621dafab64ae0ebdc5ec39a7e4400f7c9b94d
ljane <ljane@LUCYSPC>
2023-02-19 17:18:49 +0000

    Add project files.

---
+++ TSE/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TSE.Models
+{
+    public class UserModel
+    {
+        //getter and setters for the Id, username and password 
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ TSE/services/Usersdao.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data.SqlClient;
+using TSE.Models;
+
+namespace TSE.services
+{
+    public class Usersdao
+    {
+        //sets the connection string to the database string 
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog = LoginUsers; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public bool findUser (UserModel user )
+        {
+            //sets successful to false 
+            bool successful = false;
+            //defins the sql statement 
+            string sqlState = "SELECT * FROM dbo.Users WHERE Username = @username AND Password = @password";
+
+            //using the sql connection with the connection string 
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                //sets the command 
+                SqlCommand command = new SqlCommand(sqlState, connection);
+
+                //checks if the username is equal to the username in the database 
+                command.Parameters.Add("@Username", System.Data.SqlDbType.NVarChar, 50).Value = user.Username;
+                //checks if the password is equal to the username in the password 
+                command.Parameters.Add("@Password", System.Data.SqlDbType.NVarChar, 50).Value = user.Password;
+
+                //try catch statment 
+                try
+                {
+                    //opens the connection 
+                    connection.Open();
+                    //reads the sql commands
+                    SqlDataReader reader = command.ExecuteReader();
+                    //if the command has rows - means it was successful 
+                    if (reader.HasRows)
+                    {
+                        //sets successful to true 
+                        successful = true; 
+                    }
+
+                //catch execption e 
+                }catch(Exception e)
+                {
+                    //outputs the error 
+                    Console.WriteLine(e.Message);
+                }
+
+                //returns successfull 
+                return successful; 
+            }
+        }
+    }
+
+}
