4d99266871dd18158060738559fd906049f27922
JamesEastwell <james_eastwell@outlook.com>
2023-04-29 14:29:52 +0100

    Working translation with manual culture change
    currently working translation for the main page but only with a manual culture change through the URL

--- TSE/Controllers/HomeController.cs
+++ TSE/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 using System.Diagnostics;
 using TSE.Models;
 
@@ -7,14 +9,18 @@ namespace TSE.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IStringLocalizer<HomeController> _localizer;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+            IStringLocalizer<HomeController> localizer)
         {
             _logger = logger;
+            _localizer = localizer;
         }
 
         public IActionResult Index()
         {
+            var localizedTitle = _localizer["Farm Management Site"];
             return View();
         }
 
@@ -22,6 +28,16 @@ namespace TSE.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult CultureManagement(string culture)
+        {
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.Now.AddDays(30) });
+            return RedirectToAction(nameof(Index));
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
